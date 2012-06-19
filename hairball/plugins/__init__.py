6a4aaf1354f1a7369f43c90716315e41188e87f0
Bryce Boe <bbzbryce@gmail.com>
2012-06-18 23:27:07 -0700

    Added three simple plugins

--- hairball/plugins/__init__.py
+++ hairball/plugins/__init__.py
@@ -1,5 +1,11 @@
+from hashlib import sha1
+from random import random
+
 NOT_IMPL_MSG = '{0!r} needs to implement function {1!r}'
 NO_DOCSTRING = '{0!r} needs a class docstring (comment).'
+
+HTML_TMPL = """<div class="plugin_title" id="{key}">{name}</div>
+<div class="hidden" id="{key}_body">{body}</div>"""
 
 class PluginBase(object):
     def __init__(self, name, batch):
@@ -9,8 +15,12 @@ class PluginBase(object):
             raise NotImplementedError(NO_DOCSTRING.format(self.name))
         print 'Loaded {0!r}'.format(self.name)
 
-    def process(self, scratch_file):
-        raise NotImplementedError(NOT_IMPL_MSG.format(self.name, 'process'))
+    def html_wrap(self, body):
+        key = sha1(str(random())).hexdigest()
+        return HTML_TMPL.format(key=key, name=self.name, body=body)
+
+    def process(self, scratch):
+        return self.html_wrap(self._process(scratch))
 
     def finalize(self):
         raise NotImplementedError(NOT_IMPL_MSG.format(self.name, 'finalize'))
--- hairball/plugins/basic.py
+++ hairball/plugins/basic.py
@@ -7,25 +7,25 @@ class Blocks(PluginBase):
     def __init__(self, batch):
         super(Blocks, self).__init__(name='Basic Blocks', batch=batch)
 
-    def process(self, scratch_file):
-        pass
+    def _process(self, scratch):
+        return '<pre>{0}</pre>'.format(scratch.info['author'])
 
 
 class History(PluginBase):
     """Produces a display for the history contained in a scratch file."""
 
     def __init__(self, batch):
-        super(Blocks, self).__init__(name='Basic History', batch=batch)
+        super(History, self).__init__(name='Basic History', batch=batch)
 
-    def process(self, scratch_file):
-        pass
+    def _process(self, scratch):
+        return '<pre>{0}</pre>'.format(scratch.info['history'])
 
 
-class Sprites(Blocks):
+class Sprites(PluginBase):
     """Produces a visual of all sprites contained in a scratch file."""
 
     def __init__(self, batch):
-        super(Blocks, self).__init__('Basic Sprites', batch)
+        super(Sprites, self).__init__('Basic Sprites', batch=batch)
 
-    def process(self, scratch_file):
-        pass
+    def _process(self, scratch):
+        return '<p>{0}</p>'.format(len(scratch.stage.sprites))
