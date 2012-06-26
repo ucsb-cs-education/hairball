0278c926d3477cfeb925454e9e401b71cad818bc
Bryce Boe <bbzbryce@gmail.com>
2012-06-25 18:51:13 -0700

    Quick fix for plugin bug.

--- hairball/plugins/basic.py
+++ hairball/plugins/basic.py
@@ -100,7 +100,8 @@ class Changes(PluginBase):
 
     def initialization(self, sprite, property):
         for script in sprite.scripts:
-            if script.blocks[0].args[0] == 'Scratch-StartClicked':
+            if (script.blocks and
+                script.blocks[0].args[0] == 'Scratch-StartClicked'):
                 for block in script.blocks:
                     if (block.name, "relative") in property:
                         return False
