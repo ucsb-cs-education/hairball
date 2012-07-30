a8686215379c908f629a72210c060a87fc4c84a8
Charlotte Hill <charlotte@hillnetwork.com>
2012-07-30 11:11:27 -0700

    added check for empty/useless blocks to deadcode

--- hairball/plugins/blocks.py
+++ hairball/plugins/blocks.py
@@ -1,4 +1,5 @@
 import collections
+import kurt
 from . import PluginController, PluginView, PluginWrapper
 
 
@@ -67,6 +68,57 @@ class DeadCode(PluginController):
         else:
             return False
 
+    def check_empty(self, block):
+        arithmetic = set(['+', '-', '*', '/', '<', '=', '>', '\\', 'rounded'])
+        logic = set(['&', '`', 'not', 'doIf', 'doIfElse',
+                     'doForeverIf', 'doWaitUntil', 'doUntil'])
+        objects = set(['gotoSpriteOrMouse:', 'touching:', 'distanceTo:'])
+        strings = set(['think:duration:elapsed:from:', 'think:', 'say:',
+                       'say:duration:elapsed:from:', 'doAsk',
+                       'concatenate:with:', 'stringLength:',
+                       'broadcast:', 'doBroadcastAndWait', 'When I receive'])
+        if block.name == 'letter:of:':
+            if '0' in block.args or '' in block.args:
+                return True
+        elif block.name in arithmetic:
+            if block.name == 'rounded':
+                if block.args == [0]:
+                    return True
+            elif block.args == [0, 0]:
+                return True
+        elif block.name in logic:
+            if block.name == '&' or block.name == '`':
+                if block.args == [False, False]:
+                    return True
+            elif False in block.args:
+                return True
+        elif block.name in objects:
+            if None in block.args:
+                return True
+        elif block.name in strings:
+            if "" in block.args:
+                return True
+        for arg in block.args:
+            if isinstance(arg, kurt.scripts.Block):
+                return self.check_empty(arg)
+        return False
+
+    def get_useless(self, blocklist):
+        useless = []
+        for block in blocklist:
+            if self.check_empty(block):
+                useless.append(block)
+            else:
+                for arg in block.args:
+                    # lists of blocks are the stuff inside of c blocks
+                    if hasattr(arg, '__iter__'):
+                        useless.extend(self.get_useless(arg))
+                # these are parameters
+                    elif isinstance(arg, kurt.scripts.Block):
+                        if self.check_empty(block):
+                            useless.append(block)
+        return useless
+
     @PluginWrapper(html=DeadCodeView)
     def analyze(self, scratch):
         sprite_scripts = []
@@ -75,12 +127,16 @@ class DeadCode(PluginController):
             for script in sprite.scripts:
                 if script.reachable is False:
                     sprite_scripts.append(script)
+                else:
+                    sprite_scripts.extend(self.get_useless(script.blocks))
             if len(sprite_scripts) != 0:
                 sprite_dict[sprite.name] = sprite_scripts
                 sprite_scripts = []
         for script in scratch.stage.scripts:
             if script.reachable is False:
                 sprite_scripts.append(script)
+            else:
+                sprite_scripts.extend(self.get_useless(script.blocks))
         if len(sprite_scripts) != 0:
             sprite_dict["stage"] = sprite_scripts
         dynamic = self.check_dynamic(scratch)
