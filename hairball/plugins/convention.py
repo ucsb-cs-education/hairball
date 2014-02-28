f4c7c9e640b3fa4df0bb29c481ef3edb006449d8
Jesús Moreno <jesus.moreno@gonzalonazareno.org>
2014-02-28 09:20:25 +0100

    Add convention.py with plugin VariableNaming that keeps track of how often default sprites names (like Sprite1, Sprite2...) are used.

---
+++ hairball/plugins/convention.py
@@ -0,0 +1,32 @@
+"""This module provides plugins for basic programming convention checks"""
+
+from hairball.plugins import HairballPlugin
+
+
+class SpriteNaming(HairballPlugin):
+
+    """Plugin that keeps track of how often sprites default 
+       
+    names (like Sprite1, Sprite2...) are used.
+    
+    """
+
+    def __init__(self):
+        super(SpriteNaming, self).__init__()
+        self.total_default = 0
+        self.list_default = []
+        self.default_names = ["Sprite","Objeto"]
+
+    def finalize(self):
+        """Output the default sprite names found in the project."""
+        print("%d default sprite names found:" % self.total_default)
+        for name in self.list_default:
+            print name
+
+    def analyze(self, scratch):
+        """Run and return the results from the SpriteNaming plugin."""
+        for sprite in self.iter_sprites(scratch):
+            for default in self.default_names:
+                if default in sprite.name:
+                    self.total_default += 1
+                    self.list_default.append(sprite.name)
